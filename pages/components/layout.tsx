98cf5eee1a985fc821a6a23189dc32f1168ccabd
AndyAceHuang <andy.huang@akqa.com>
2023-04-26 15:30:37 +1200

    added styled components

---
+++ pages/components/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
