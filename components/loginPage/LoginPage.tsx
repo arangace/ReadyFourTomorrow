88afe84e32175d1d7257695e326038cb659efba6
AndyAceHuang <andy.huang@akqa.com>
2023-04-28 18:24:37 +1200

    changed loginform imports

--- components/loginPage/LoginPage.tsx
+++ components/loginPage/LoginPage.tsx
@@ -2,22 +2,19 @@ import styled from "styled-components";
 import { Container, MainHeader, Wrapper, LoginButton } from "./LoginStyles";
 import LoginForm from "../loginForm/LoginForm";
 import { signIn } from "next-auth/react";
-import { Suspense } from "react";
 type LoginPageInformation = { loginButtonHandler: () => void };
 
 const LoginPage = ({ loginButtonHandler }: LoginPageInformation) => {
   return (
-    <Suspense fallback={<div>I am loading..</div>}>
-      <Container>
-        <Wrapper>
-          <MainHeader>Login</MainHeader>
-          <LoginForm loginButtonHandler={loginButtonHandler}></LoginForm>
-          <LoginButton onClick={() => signIn("google")}>
-            Sign in with gooogle
-          </LoginButton>
-        </Wrapper>
-      </Container>
-    </Suspense>
+    <Container>
+      <Wrapper>
+        <MainHeader>Login</MainHeader>
+        <LoginForm loginButtonHandler={loginButtonHandler}></LoginForm>
+        <LoginButton onClick={() => signIn("google")}>
+          Sign in with gooogle
+        </LoginButton>
+      </Wrapper>
+    </Container>
   );
 };
 
