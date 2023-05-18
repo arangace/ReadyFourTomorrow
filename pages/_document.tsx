acb04adf379dffe376e29fa5047d4765c9604100
AndyAceHuang <andy.huang@akqa.com>
2023-05-19 09:32:12 +1200

    added google site verification tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,6 +41,10 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/images/R4T-logo.png"></link>
           <meta name="theme-color" content="#7056b3" />
+          <meta
+            name="google-site-verification"
+            content="7OvIkcZsysGa00luWu-y86lmr40xI7BDf71kMIP9XrI"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
